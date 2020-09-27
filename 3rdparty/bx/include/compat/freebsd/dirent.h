376e24ad5206c7e442b18dc682a26c58477769b2
Emmanuel Vadot <manu@FreeBSD.org>
2020-09-27 10:44:47 +0200

    3rdparty/bx: FreeBSD: Fix includes
    For both dirent.h and signal.h we need to use the userland version,
    directly including the sys/ ones cannot be done.
    
    TODO: Upstream
    
    Signed-off-by: Emmanuel Vadot <manu@FreeBSD.org>

--- 3rdparty/bx/include/compat/freebsd/dirent.h
+++ 3rdparty/bx/include/compat/freebsd/dirent.h
@@ -1,1 +1,1 @@
-#include <sys/dirent.h>
+#include_next <dirent.h>
--- 3rdparty/bx/include/compat/freebsd/signal.h
+++ 3rdparty/bx/include/compat/freebsd/signal.h
@@ -1,5 +1,9 @@
 #if defined(__GLIBC__)
 #	include_next <signal.h>
 #else
+#if defined(__FreeBSD__)
+#	include_next <signal.h>
+#else
 #	include <sys/signal.h>
+#endif
 #endif
